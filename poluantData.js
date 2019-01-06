83253314b6f918518c4b14e82069eabfe1c31736
unknown <alinmihai97@gmail.com>
2019-01-06 02:46:50 +0200

    major changes

---
+++ poluantData.js
@@ -0,0 +1,63 @@
+var poluantLimits = [
+    {
+        name: "COX",
+        limit: 15
+    }
+]
+var poluantData = [
+    {
+        areaId: 1,
+        poluants: [
+            {
+                name: "COX",
+                values: [3, 4, 8, 5, 18, 13, 10]
+            }
+        ]
+    },
+    {
+        areaId: 2,
+        poluants: [
+            {
+                name: "COX",
+                values: [14, 5, 1, 0, 16, 6, 18]
+            }
+        ]
+    },
+    {
+        areaId: 3,
+        poluants: [
+            {
+                name: "COX",
+                values: [16, 11, 15, 1, 8, 9, 19]
+            }
+        ]
+    },
+    {
+        areaId: 4,
+        poluants: [
+            {
+                name: "COX",
+                values: [9, 13, 19, 10, 1, 17, 3]
+            }
+        ]
+    },
+    {
+        areaId: 5,
+        poluants: [
+            {
+                name: "COX",
+                values: [17, 4, 0, 12, 7, 6, 18]
+            }
+        ]
+    },
+    {
+        areaId: 6,
+        poluants: [
+            {
+                name: "COX",
+                values: [15, 0, 16, 17, 18, 12, 19]
+            }
+        ]
+    }
+
+]
