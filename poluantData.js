2eda16d4238cef3a4ffda74bcf5f41733c636a8f
unknown <alinmihai97@gmail.com>
2019-01-11 18:35:56 +0200

    new feature

--- poluantData.js
+++ poluantData.js
@@ -1,16 +1,45 @@
 var poluantLimits = [
     {
-        name: "COX",
-        limit: 15
+        name: "Detergenti",
+        limit: 5,
+        unit: "mg/l"
+    },
+    {
+        name: "Mercur",
+        limit: 13.5,
+        unit: "mg/l"
+    },
+    {
+        name: "Plastic",
+        limit: 0.3,
+        unit: "kg/zi"
+    },
+    {
+        name: "Sulfati",
+        limit: 200,
+        unit: "mg/l"
     }
+
 ]
 var poluantData = [
     {
         areaId: 1,
         poluants: [
             {
-                name: "COX",
-                values: [3, 4, 8, 5, 18, 13, 10]
+                name: "Detergenti",
+                values: [2.2, 1.1, 1.5, 1.9, 4.8, 1.3, 4.2]
+            },
+            {
+                name: "Mercur",
+                values: [2.0, 4.7, 2.2, 6.4, 3.3, 4.4, 7.1]
+            },
+            {
+                name: "Plastic",
+                values: [0.62, 0.106, 0.194, 0.236, 0.111, 0.269, 0.189]
+            },
+            {
+                name: "Sulfati",
+                values: [9, 36, 40, 97, 103, 134, 198]
             }
         ]
     },
@@ -18,8 +47,20 @@ var poluantData = [
         areaId: 2,
         poluants: [
             {
-                name: "COX",
-                values: [14, 5, 1, 0, 16, 6, 18]
+                name: "Detergenti",
+                values: [4.5, 4.8, 2.4, 4.4, 3.7, 0.1, 0.5]
+            },
+            {
+                name: "Mercur",
+                values: [7.1, 1.7, 3.3, 6.9, 5.1, 7.4, 2.6]
+            },
+            {
+                name: "Plastic",
+                values: [0.86, 0.188, 0.20, 0.283, 0.62, 0.263, 0.1]
+            },
+            {
+                name: "Sulfati",
+                values: [3, 17, 55, 70, 111, 129, 191]
             }
         ]
     },
@@ -27,8 +68,20 @@ var poluantData = [
         areaId: 3,
         poluants: [
             {
-                name: "COX",
-                values: [16, 11, 15, 1, 8, 9, 19]
+                name: "Detergenti",
+                values: [3.9, 2.2, 0.4, 1.3, 2.0, 2.3, 0]
+            },
+            {
+                name: "Mercur",
+                values: [0.1, 7.0, 4.6, 6.1, 6.8, 5.6, 6.2]
+            },
+            {
+                name: "Plastic",
+                values: [0.119, 0.250, 0.14, 0.181, 0.64, 0.62, 0.150]
+            },
+            {
+                name: "Sulfati",
+                values: [22, 57, 90, 113, 132, 166, 195]
             }
         ]
     },
@@ -36,8 +89,21 @@ var poluantData = [
         areaId: 4,
         poluants: [
             {
-                name: "COX",
-                values: [9, 13, 19, 10, 1, 17, 3]
+                name: "Detergenti",
+                values: [1.6, 2.2, 2.9, 0.9, 1.5, 2.5, 4.2]
+            }
+            ,
+            {
+                name: "Mercur",
+                values: [7.0, 4.3, 2.9, 5.9, 5.8, 6.1, 7.4]
+            },
+            {
+                name: "Plastic",
+                values: [0.38, 0.65, 0.66, 0.205, 0.283, 0.181, 0.146]
+            },
+            {
+                name: "Sulfati",
+                values: [7, 63, 95, 105, 108, 160, 174]
             }
         ]
     },
@@ -45,8 +111,20 @@ var poluantData = [
         areaId: 5,
         poluants: [
             {
-                name: "COX",
-                values: [17, 4, 0, 12, 7, 6, 18]
+                name: "Detergenti",
+                values: [2.8, 4.8, 4.9, 0.2, 0.8, 3.4, 4.2]
+            },
+            {
+                name: "Mercur",
+                values: [1.2, 4.4, 1.6, 4.0, 0, 4.5, 0.2]
+            },
+            {
+                name: "Plastic",
+                values: [0.34, 0.146, 0.215, 0.36, 0.122, 0.289, 0.241]
+            },
+            {
+                name: "Sulfati",
+                values: [19, 34, 60, 66, 86, 124, 200]
             }
         ]
     },
@@ -54,8 +132,20 @@ var poluantData = [
         areaId: 6,
         poluants: [
             {
-                name: "COX",
-                values: [15, 0, 16, 17, 18, 12, 19]
+                name: "Detergenti",
+                values: [1.2, 2.5, 0.2, 4.7, 1.7, 3.5, 0.8]
+            },
+            {
+                name: "Mercur",
+                values: [3.5, 0.2, 6.1, 6.9, 5.8, 3.3, 3.9]
+            },
+            {
+                name: "Plastic",
+                values: [0.200, 0.238, 0.205, 0.241, 0.234, 0.139, 0.70]
+            },
+            {
+                name: "Sulfati",
+                values: [6, 36, 78, 81, 90, 104, 124]
             }
         ]
     }
